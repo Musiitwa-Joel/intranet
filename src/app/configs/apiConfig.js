a5e287976498c445da1374398c504e8f766b060f
Musiitwa-Joel <83823328+Musiitwa-Joel@users.noreply.github.com>
2025-03-13 19:46:33 +0300

    hi

--- src/app/configs/apiConfig.js
+++ src/app/configs/apiConfig.js
@@ -1,7 +1,7 @@
 // const mainUrl =
 //   "http://ec2-3-147-67-81.us-east-2.compute.amazonaws.com:2323/graphql";
-const mainUrl = "http://localhost:2323/graphql";
-const webSocketUrl = "ws://localhost:2323/graphql";
-const url2 = "http://localhost:2222";
+const mainUrl = "http://192.168.43.181:2323/graphql";
+const webSocketUrl = "ws://192.168.43.181:2323/graphql";
+const url2 = "http://192.168.43.181:2222";
 
 export { mainUrl, url2, webSocketUrl };
